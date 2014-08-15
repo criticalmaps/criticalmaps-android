a2823a2ea3eb1b41beb0072c07a7206abb94b3a2
stephan lindauer <stephan.lindauer@gmail.com>
2014-08-15 23:14:12 +0200

    map marker for own position moves now

--- src/de/stephanlindauer/criticalmass_berlin/fragments/MapFragment.java
+++ src/de/stephanlindauer/criticalmass_berlin/fragments/MapFragment.java
@@ -1,9 +1,14 @@
 package de.stephanlindauer.criticalmass_berlin.fragments;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.*;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.RelativeLayout;
@@ -19,6 +24,14 @@ import org.osmdroid.views.overlay.OverlayItem;
 import java.util.ArrayList;
 
 public class MapFragment extends Fragment {
+
+    private static final float LOCATION_REFRESH_DISTANCE = 5; //meters
+    private static final long LOCATION_REFRESH_TIME = 10000; //milliseconds
+
+    private MapView mapView;
+    private ItemizedIconOverlay<OverlayItem> myLocationOverlay;
+
+    private FragmentActivity mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,9 +69,9 @@ public class MapFragment extends Fragment {
     @Override
     public void onActivityCreated(final Bundle savedState) {
         super.onActivityCreated(savedState);
-        FragmentActivity mContext = getActivity();
+        mContext = getActivity();
 
-        final MapView mapView = new MapView(mContext, null);
+        mapView = new MapView(mContext, null);
 
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setBuiltInZoomControls(true);
@@ -82,7 +95,8 @@ public class MapFragment extends Fragment {
                 LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
         ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
-        overlays.add(new OverlayItem("New Overlay", "Overlay Description", new GeoPoint((int) (52.468190 * 1E6), (int) (13.426046 * 1E6)) ));
+
+        overlays.add(new OverlayItem("New Overlay", "Overlay Description", new GeoPoint((int) (52.468190 * 1E6), (int) (13.426046 * 1E6))));
 
         DefaultResourceProxyImpl resourceProxy = new DefaultResourceProxyImpl(mContext);
         ItemizedIconOverlay<OverlayItem> myLocationOverlay = new ItemizedIconOverlay<OverlayItem>(overlays, getResources().getDrawable(R.drawable.map_marker), null, resourceProxy);
@@ -92,5 +106,48 @@ public class MapFragment extends Fragment {
 
         RelativeLayout RL = (RelativeLayout) getActivity().findViewById(R.id.relativeLayout);
         RL.addView(mapView);
+
+        LocationManager mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+
     }
+    private final LocationListener mLocationListener = new LocationListener() {
+
+        private ItemizedIconOverlay<OverlayItem> myLocationOverlay1;
+
+        @Override
+        public void onLocationChanged(final Location location) {
+
+            GeoPoint currentUserLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
+            ArrayList<OverlayItem> ownOverlay = new ArrayList<OverlayItem>();
+            ownOverlay.add(new OverlayItem("", "", currentUserLocation));
+
+            DefaultResourceProxyImpl resourceProxy = new DefaultResourceProxyImpl(mContext);
+
+            ItemizedIconOverlay<OverlayItem> oldMyLocationOverlay = myLocationOverlay;
+            myLocationOverlay = new ItemizedIconOverlay<OverlayItem>(ownOverlay, getResources().getDrawable(R.drawable.map_marker_own), null, resourceProxy);
+            mapView.getOverlays().add(myLocationOverlay);
+
+            mapView.getOverlays().remove( oldMyLocationOverlay );
+
+            mapView.invalidate();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 }
