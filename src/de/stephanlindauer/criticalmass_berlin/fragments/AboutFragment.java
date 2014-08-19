bdc0995fb146cab0060b607b7afb1a9c484ed417
stephan lindauer <stephan.lindauer@gmail.com>
2014-08-20 01:35:30 +0200

    about section done for now

--- src/de/stephanlindauer/criticalmass_berlin/fragments/AboutFragment.java
+++ src/de/stephanlindauer/criticalmass_berlin/fragments/AboutFragment.java
@@ -1,15 +1,19 @@
 package de.stephanlindauer.criticalmass_berlin.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import de.stephanlindauer.criticalmass_berlin.R;
 
 public class AboutFragment extends SuperFragment {
 
     private FragmentActivity activity;
+    private Intent websiteIntent;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,5 +27,44 @@ public class AboutFragment extends SuperFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        Button paypalButton = (Button) getActivity().findViewById(R.id.about_paypal);
+        Button facebookButton = (Button) getActivity().findViewById(R.id.about_facebook);
+        Button twitterButton = (Button) getActivity().findViewById(R.id.about_twitter);
+
+        paypalButton.setOnClickListener(clickListerner);
+        facebookButton.setOnClickListener(clickListerner);
+        twitterButton.setOnClickListener(clickListerner);
     }
+
+    private View.OnClickListener clickListerner = new View.OnClickListener() {
+        public void onClick(View view) {
+            String url = getUrlForId(view.getId());
+            Intent websiteIntent = new Intent(android.content.Intent.ACTION_VIEW);
+            websiteIntent.setData(Uri.parse(url));
+            startActivity(websiteIntent);
+        }
+    };
+
+    private String getUrlForId(int id) {
+        String url = "";
+
+        switch (id) {
+            case R.id.about_paypal:
+                url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=8DBW7Z768DQT6&lc=GB&no_note=0&cn=Mitteilung%20an%20den%20H%c3%a4ndler%3a&no_shipping=1&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
+                break;
+            case R.id.about_facebook:
+                url = "https://www.facebook.com/pages/Critical-Mass-Berlin/74806304846";
+                break;
+            case R.id.about_twitter:
+                url = "https://twitter.com/CMBerlin";
+                break;
+            default:
+                url = "google.de";
+                break;
+        }
+
+        return url;
+    }
+
 }
