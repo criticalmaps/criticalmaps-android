def36b84acd8d0dd762bcf3827b4d626dd35add2
stephan lindauer <stephan.lindauer@gmail.com>
2014-09-15 22:00:40 +0200

    refactoring

--- src/de/stephanlindauer/criticalmass/SplashScreen.java
+++ src/de/stephanlindauer/criticalmass/SplashScreen.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 
 public class SplashScreen extends Activity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_DURATION = 1000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,10 @@ public class SplashScreen extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashScreen.this, Main.class);
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }, SPLASH_DISPLAY_DURATION);
     }
 }
