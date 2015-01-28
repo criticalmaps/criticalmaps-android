35935e2bd146d94cd8c4c1addee3b10cca427d4b
stephan lindauer <stephan.lindauer@gmail.com>
2015-01-28 07:26:01 +0100

    gps string holder

---
+++ src/de/stephanlindauer/criticalmaps/model/SternfahrtModel.java
@@ -0,0 +1,23 @@
+package de.stephanlindauer.criticalmaps.model;
+
+public class SternfahrtModel {
+    public static String werder = "";
+    public static String brandenburg = "";
+    public static String nauen = "";
+    public static String heiligensee = "";
+    public static String frohnau = "";
+    public static String jungfernheide = "";
+    public static String oranienburg = "";
+    public static String wandlitzsee = "";
+    public static String eberswalde = "";
+    public static String ahrensfelde = "";
+    public static String strausberg = "";
+    public static String frankfurt = "";
+    public static String koenigsWusterhausen = "";
+    public static String zossen = "";
+    public static String altTempelhof = "";
+    public static String bundesplatz = "";
+    public static String ludwigsfelde = "";
+    public static String potsdam = "";
+    public static String kinderroute = "";
+}
