163ce80ad1afe681cd50d9224a652c392b10dc27
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-13 23:06:32 +0200

    null pointer fix

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/LocationUtils.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/LocationUtils.java
@@ -1,30 +1,39 @@
 package de.stephanlindauer.criticalmaps.utils;
 
+import android.location.Location;
 import android.location.LocationManager;
 
 import org.osmdroid.util.GeoPoint;
 
 public class LocationUtils {
     public static GeoPoint getBestLastKnownLocation(LocationManager locationManager) {
-        GeoPoint lastKnownLocation = null;
-
-        if (locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
-            lastKnownLocation = new GeoPoint(
-                    locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER).getLatitude(),
-                    locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER).getLongitude());
+        if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER) && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Location lastKnownLocationGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnownLocationGps != null) {
+                return new GeoPoint(
+                        lastKnownLocationGps.getLatitude(),
+                        lastKnownLocationGps.getLongitude());
+            }
         }
 
-        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            lastKnownLocation = new GeoPoint(
-                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).getLatitude(),
-                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).getLongitude());
+        if (locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER) && locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            Location lastKnownLocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (lastKnownLocationNetwork != null) {
+                return new GeoPoint(
+                        lastKnownLocationNetwork.getLatitude(),
+                        lastKnownLocationNetwork.getLongitude());
+            }
         }
 
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            lastKnownLocation = new GeoPoint(
-                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude(),
-                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude());
+        if (locationManager.getAllProviders().contains(LocationManager.PASSIVE_PROVIDER) && locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            Location lastKnownLocationPassive = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            if (lastKnownLocationPassive != null) {
+                return new GeoPoint(
+                        lastKnownLocationPassive.getLatitude(),
+                        lastKnownLocationPassive.getLongitude());
+            }
         }
-        return lastKnownLocation;
+
+        return null;
     }
 }
