7f2b6b27a82942cb46ae61b77329a329f489a426
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-16 15:23:01 +0200

    swipe to refresh twitter

--- app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
@@ -1,11 +1,13 @@
 package de.stephanlindauer.criticalmaps.fragments;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,9 +25,12 @@ public class TwitterFragment extends SuperFragment {
     //view
     private View twitterView;
     private ProgressBar loadingSpinner;
-
+    private SwipeRefreshLayout swipeLayout;
     //adapter
     private TweetAdapter tweetAdapter;
+
+    //misc
+    private final TwitterFragment thiz = this;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,10 +50,24 @@ public class TwitterFragment extends SuperFragment {
         ListView tweetListView = (ListView) getActivity().findViewById(R.id.tweet_list);
         tweetListView.setAdapter(tweetAdapter);
 
+        swipeLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new TwitterGetHandler(thiz).execute();
+                Toast.makeText(getActivity(), R.string.twitter_refreshed, Toast.LENGTH_SHORT).show();
+            }
+        });
+        swipeLayout.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.darker_gray,
+                android.R.color.holo_blue_bright,
+                android.R.color.darker_gray);
+
         new TwitterGetHandler(this).execute();
     }
 
     public void displayNewData() {
+        swipeLayout.setRefreshing(false);
         loadingSpinner.setVisibility(View.GONE);
 
         tweetAdapter.clear();
