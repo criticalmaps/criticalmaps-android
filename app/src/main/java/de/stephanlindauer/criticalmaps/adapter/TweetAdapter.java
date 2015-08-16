8ff11523569c155ba41c289715ede028e7933727
stephan lindauer <stephan.lindauer@gmail.com>
2015-08-16 03:33:08 +0200

    twitter integration

---
+++ app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TweetAdapter.java
@@ -0,0 +1,76 @@
+package de.stephanlindauer.criticalmaps.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import de.stephanlindauer.criticalmaps.R;
+import de.stephanlindauer.criticalmaps.helper.TimeToWordStringConverter;
+import de.stephanlindauer.criticalmaps.interfaces.IChatMessage;
+import de.stephanlindauer.criticalmaps.vo.chat.OutgoingChatMessage;
+import de.stephanlindauer.criticalmaps.vo.chat.ReceivedChatMessage;
+
+public class TweetAdapter extends ArrayAdapter<IChatMessage> {
+
+    private ArrayList<IChatMessage> chatMessages;
+    private Context context;
+
+
+    public TweetAdapter(Context context, int layoutResourceId, ArrayList<IChatMessage> chatMessages) {
+        super(context, layoutResourceId, chatMessages);
+        this.chatMessages = chatMessages;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        IChatMessage currentMessage = chatMessages.get(position);
+
+        if (currentMessage instanceof ReceivedChatMessage)
+            return buildReceivedMessageView((ReceivedChatMessage) currentMessage, inflater, parent);
+        else if (currentMessage instanceof OutgoingChatMessage)
+            return buildOutgoingMessageView((OutgoingChatMessage) currentMessage, inflater, parent);
+
+        return null;
+    }
+
+    private View buildOutgoingMessageView(OutgoingChatMessage currentMessage, LayoutInflater inflater, ViewGroup parent) {
+
+        View rowView = inflater.inflate(R.layout.view_outgoing_chatmessage, parent, false);
+
+        TextView labelView = (TextView) rowView.findViewById(R.id.firstLine);
+        TextView valueView = (TextView) rowView.findViewById(R.id.secondLine);
+
+        labelView.setText(R.string.chat_sending);
+        valueView.setText(currentMessage.getMessage());
+
+        return rowView;
+    }
+
+    private View buildReceivedMessageView(ReceivedChatMessage currentMessage, LayoutInflater inflater, ViewGroup parent) {
+
+        View rowView = inflater.inflate(R.layout.view_chatmessage, parent, false);
+
+        TextView labelView = (TextView) rowView.findViewById(R.id.firstLine);
+        TextView valueView = (TextView) rowView.findViewById(R.id.secondLine);
+
+        DateFormat dateFormatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT, Locale.getDefault());
+        dateFormatter.setTimeZone(TimeZone.getDefault());
+
+        labelView.setText(TimeToWordStringConverter.getTimeAgo(currentMessage.getTimestamp(), context));
+        valueView.setText(currentMessage.getMessage());
+
+        return rowView;
+    }
+}
--- app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
@@ -2,12 +2,17 @@ package de.stephanlindauer.criticalmaps.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import de.stephanlindauer.criticalmaps.R;
 import de.stephanlindauer.criticalmaps.adapter.ChatMessageAdapter;
 import de.stephanlindauer.criticalmaps.handler.TwitterGetHandler;
+import de.stephanlindauer.criticalmaps.vo.chat.OutgoingChatMessage;
 
 public class TwitterFragment extends SuperFragment {
 
@@ -29,5 +34,56 @@ public class TwitterFragment extends SuperFragment {
     public void onActivityCreated(final Bundle savedState) {
         super.onActivityCreated(savedState);
         new TwitterGetHandler().execute();
+
+        super.onActivityCreated(savedState);
+
+        chatMessageAdapter = new ChatMessageAdapter(getActivity(), R.layout.view_chatmessage, chatModel.getSavedAndOutgoingMessages());
+
+        chatListView = (ListView) getActivity().findViewById(R.id.chat_list);
+        chatListView.setAdapter(chatMessageAdapter);
+
+        chatMessageAdapter.notifyDataSetChanged();
+
+        chatListView.setSelection(chatListView.getCount());
+
+        chatListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_MOVE:
+                        isScrolling = true;
+                        return false;
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP:
+                        isScrolling = false;
+                        return false;
+                }
+                return false;
+            }
+        });
+
+        editMessageTextfield = (EditText) getActivity().findViewById(R.id.chat_edit_message);
+
+        Button sendButton = (Button) getActivity().findViewById(R.id.chat_send_btn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editMessageTextfield.getText().toString();
+
+                if (message.equals(""))
+                    return;
+
+                chatModel.setNewOutgoingMessage(new OutgoingChatMessage(message));
+
+                editMessageTextfield.setText("");
+                chatMessageAdapter = new ChatMessageAdapter(getActivity(), 123, chatModel.getSavedAndOutgoingMessages());
+                chatListView.setAdapter(chatMessageAdapter);
+
+                if (!isScrolling)
+                    chatListView.setSelection(chatListView.getCount());
+            }
+        });
+
     }
 }
