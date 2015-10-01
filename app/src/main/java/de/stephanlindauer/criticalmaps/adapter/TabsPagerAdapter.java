6f96e1f70c2cc26fd5ccec816939eddfd582d879
Christian Balster <christian.balster@gmail.com>
2015-10-01 20:46:17 +0200

    Switch from ActionBar to TabLayout
    The main changes here are replacing FragmentActivity with
    AppCompatActivity (appcompat v7) and switching from ActionBar
    with NAVIGATION_MODE_TABS to TabLayout (design v4).
    This also adds a hint of material design and can be a
    first step to "materialization" of the app.

--- app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TabsPagerAdapter.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TabsPagerAdapter.java
@@ -1,9 +1,12 @@
 package de.stephanlindauer.criticalmaps.adapter;
 
+import android.app.Application;
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import de.stephanlindauer.criticalmaps.R;
 import de.stephanlindauer.criticalmaps.fragments.AboutFragment;
 import de.stephanlindauer.criticalmaps.fragments.ChatFragment;
 import de.stephanlindauer.criticalmaps.fragments.MapFragment;
@@ -12,8 +15,11 @@ import de.stephanlindauer.criticalmaps.fragments.TwitterFragment;
 
 public class TabsPagerAdapter extends FragmentPagerAdapter {
 
-    public TabsPagerAdapter(FragmentManager fm) {
+    private final Application application;
+
+    public TabsPagerAdapter(Application application, FragmentManager fm) {
         super(fm);
+        this.application = application;
     }
 
     @Override
@@ -29,13 +35,32 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
                 return new RulesFragment();
             case 4:
                 return new AboutFragment();
+            default:
+                return null;
         }
-
-        return null;
     }
 
     @Override
     public int getCount() {
         return 5;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Resources res = application.getResources();
+        switch (position) {
+            case 0:
+                return res.getString(R.string.section_map);
+            case 1:
+                return res.getString(R.string.section_chat);
+            case 2:
+                return res.getString(R.string.section_twitter);
+            case 3:
+                return res.getString(R.string.section_rules);
+            case 4:
+                return res.getString(R.string.section_about);
+            default:
+                return null;
+        }
     }
 }
