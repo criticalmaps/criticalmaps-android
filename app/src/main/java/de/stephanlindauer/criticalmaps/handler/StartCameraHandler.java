5da0d676335106d67faf9b2473dd5eeb54904850
ligi <ligi@ligi.de>
2015-10-13 17:17:45 +0200

    Cleanup

--- app/src/main/java/de/stephanlindauer/criticalmaps/handler/StartCameraHandler.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/handler/StartCameraHandler.java
@@ -1,15 +1,11 @@
 package de.stephanlindauer.criticalmaps.handler;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
-
 import android.support.v7.app.AlertDialog;
-import java.io.File;
-
 import de.stephanlindauer.criticalmaps.Main;
 import de.stephanlindauer.criticalmaps.R;
 import de.stephanlindauer.criticalmaps.model.OwnLocationModel;
@@ -17,13 +13,13 @@ import de.stephanlindauer.criticalmaps.utils.ImageUtils;
 import de.stephanlindauer.criticalmaps.vo.RequestCodes;
 import de.stephanlindauer.criticalmaps.vo.ResultType;
 
+import java.io.File;
+
 public class StartCameraHandler extends AsyncTask<Void, Void, ResultType> {
 
-    private File outputFile;
-    private Activity activity;
+    private Main activity;
 
-
-    public StartCameraHandler(Activity mainActivity) {
+    public StartCameraHandler(Main mainActivity) {
         this.activity = mainActivity;
     }
 
@@ -47,12 +43,12 @@ public class StartCameraHandler extends AsyncTask<Void, Void, ResultType> {
 
     @Override
     protected ResultType doInBackground(Void... voids) {
-        outputFile = ImageUtils.getNewOutputImageFile();
-        ((Main)activity).setNewCameraOutputFile(outputFile); // FIXME: 07.09.2015
+        final File outputFile = ImageUtils.getNewOutputImageFile();
+        activity.setNewCameraOutputFile(outputFile); // FIXME: 07.09.2015
 
         PackageManager packageManager = activity.getPackageManager();
 
-        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA) == false) {
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             return ResultType.FAILED;
         }
 
