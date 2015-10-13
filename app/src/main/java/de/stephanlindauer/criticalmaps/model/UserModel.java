93b5e580f0c996de45842d5d1e25f7abfa851aeb
ligi <ligi@ligi.de>
2015-10-13 17:20:04 +0200

    Break down really long line

--- app/src/main/java/de/stephanlindauer/criticalmaps/model/UserModel.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/model/UserModel.java
@@ -2,8 +2,9 @@ package de.stephanlindauer.criticalmaps.model;
 
 import android.app.Activity;
 import android.provider.Settings;
-
 import de.stephanlindauer.criticalmaps.utils.AeSimpleSHA1;
+
+import static android.provider.Settings.Secure.getString;
 
 public class UserModel {
 
@@ -20,8 +21,8 @@ public class UserModel {
     }
 
     public void initialize(Activity activity) {
-        this.uniqueDeviceIdHashed = AeSimpleSHA1.SHA1(Settings.Secure.getString(activity.getContentResolver(),
-                Settings.Secure.ANDROID_ID));
+        final String androidId = getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
+        uniqueDeviceIdHashed = AeSimpleSHA1.SHA1(androidId);
     }
 
     public String getUniqueDeviceIdHashed() {
