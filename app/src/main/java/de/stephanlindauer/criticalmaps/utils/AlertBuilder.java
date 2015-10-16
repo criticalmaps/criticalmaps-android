4a1d74138609d89fdce3cca5e3aa994568c80439
Christian Balster <christian.balster@gmail.com>
2015-10-16 13:34:33 +0200

    define theme for Alert Dialog and use it
    v7 AlertDialog doesn't automatically inherit the app's theme
    also adds support annotations to the AlertBuilder

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/AlertBuilder.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/AlertBuilder.java
@@ -2,12 +2,13 @@ package de.stephanlindauer.criticalmaps.utils;
 
 import android.app.Activity;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import de.stephanlindauer.criticalmaps.R;
 
 public class AlertBuilder {
-    public static void show(Activity activity, int title, int text) {
-        new AlertDialog.Builder(activity)
+    public static void show(Activity activity, @StringRes int title, @StringRes int text) {
+        new AlertDialog.Builder(activity, R.style.AlertDialogTheme)
                 .setTitle(activity.getString(title))
                 .setMessage(activity.getString(text))
                 .setCancelable(false)
