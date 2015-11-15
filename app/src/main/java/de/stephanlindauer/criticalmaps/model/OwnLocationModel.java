e8b6664eb497dc03a8566c8b980fc71d52bbdf27
Christian Balster <christian.balster@gmail.com>
2015-11-15 20:03:32 +0100

    don't send old location fixes to backend

--- app/src/main/java/de/stephanlindauer/criticalmaps/model/OwnLocationModel.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/model/OwnLocationModel.java
@@ -6,20 +6,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.osmdroid.util.GeoPoint;
 
+import java.util.Date;
+
+import de.stephanlindauer.criticalmaps.utils.DateUtils;
+
 public class OwnLocationModel {
 
+    public static final int MAX_LOCATION_AGE = 30 * 1000; //30 seconds
     private static final float ACCURACY_PRECISE_THRESHOLD = 50.0f; //meters
 
     public GeoPoint ownLocation;
     private boolean isLocationPrecise;
+    private long timeOfFix;
 
-    public void setLocation(@NonNull GeoPoint location, float accuracy) {
+    public void setLocation(@NonNull GeoPoint location, float accuracy, long time) {
         ownLocation = location;
         isLocationPrecise = (accuracy < ACCURACY_PRECISE_THRESHOLD);
+        timeOfFix = time;
     }
 
     public boolean hasPreciseLocation() {
         return (ownLocation != null) && isLocationPrecise;
+    }
+
+    public boolean isLocationFresh() {
+        return DateUtils.isNotLongerAgoThen(new Date(timeOfFix), 0, 30);
     }
 
     @NonNull
