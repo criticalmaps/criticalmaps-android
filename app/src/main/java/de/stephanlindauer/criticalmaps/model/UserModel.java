dbe6bb1dbd1ede6c9bffd05f4da14807b7a388ae
stephan lindauer <stephan.lindauer@gmail.com>
2015-12-09 00:31:02 +0100

    added salt to uniqueDeviceId

--- app/src/main/java/de/stephanlindauer/criticalmaps/model/UserModel.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/model/UserModel.java
@@ -2,20 +2,25 @@ package de.stephanlindauer.criticalmaps.model;
 
 import android.app.Activity;
 import android.provider.Settings;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import de.stephanlindauer.criticalmaps.utils.AeSimpleSHA1;
 
 import static android.provider.Settings.Secure.getString;
 
 public class UserModel {
 
-    private String uniqueDeviceIdHashed;
+    private String uniqueDeviceId;
 
     public void initialize(Activity activity) {
         final String androidId = getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
-        uniqueDeviceIdHashed = AeSimpleSHA1.SHA1(androidId);
+        final String dateString = new SimpleDateFormat("yyyy-mm-dd").format(new Date());
+        uniqueDeviceId = AeSimpleSHA1.SHA1(androidId + dateString);
     }
 
-    public String getUniqueDeviceIdHashed() {
-        return uniqueDeviceIdHashed;
+    public String getUniqueDeviceId() {
+        return uniqueDeviceId;
     }
 }
