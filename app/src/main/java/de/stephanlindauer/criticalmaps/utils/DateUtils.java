ab17322f422582e7d73be72e2cf7f3e99de4d35c
George Kankava <george.kankava@devfactory.com>
2016-03-16 22:15:22 +0400

    squid:S1118 - Utility classes should not have public constructors

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import java.util.Date;
 
 public class DateUtils {
+
+    private DateUtils() {}
+
     public static boolean isNotLongerAgoThen(@NonNull Date timestamp, int minutes, int seconds) {
         long differenceInMs = timestamp.getTime() - new Date().getTime();
         return differenceInMs <= (minutes * 60 + seconds) * 1000;
