77ee0cc726ef042f8377f5b327b8d591354b886d
stephan lindauer <s.lindauer@rebuy.com>
2017-06-05 14:30:40 +0200

    temporarily change endpoints to own domain

--- app/src/main/java/de/stephanlindauer/criticalmaps/vo/Endpoints.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/vo/Endpoints.java
@@ -1,7 +1,7 @@
 package de.stephanlindauer.criticalmaps.vo;
 
 public class Endpoints {
-    public static final String MAIN_POST = "http://api.criticalmaps.net/postv2";
-    public static final String IMAGE_POST = "http://api.criticalmaps.net/gallery/post.php";
-    public static final String GET_TWITTER = "http://api.criticalmaps.net/twitter/get.php";
+    public static final String MAIN_POST = "https://criticalmaps-api.stephanlindauer.de/postv2";
+    public static final String IMAGE_POST = "https://criticalmaps-api.stephanlindauer.de/gallery/post.php";
+    public static final String GET_TWITTER = "https://criticalmaps-api.stephanlindauer.de/twitter/get.php";
 }
