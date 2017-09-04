2eb844738df6100c49349202d6ec7b435a4d4839
Christian Balster <christian.balster@gmail.com>
2017-09-04 22:45:15 +0200

    DateUtils: fix time always being reported as new
    also change parameter name to reflect expected input

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
@@ -8,8 +8,8 @@ public class DateUtils {
 
     private DateUtils() {}
 
-    public static boolean isNotLongerAgoThen(@NonNull Date timestamp, int minutes, int seconds) {
-        long differenceInMs = timestamp.getTime() - new Date().getTime();
+    public static boolean isNotLongerAgoThen(@NonNull Date date, int minutes, int seconds) {
+        long differenceInMs = new Date().getTime() - date.getTime();
         return differenceInMs <= (minutes * 60 + seconds) * 1000;
     }
 }
