8efe1e932425edfb88d3ac53a5ae8dcc81abd5d5
Christian Balster <christian.balster@gmail.com>
2018-10-06 12:22:55 +0200

    Fix conversion error

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/DateUtils.java
@@ -10,6 +10,6 @@ public class DateUtils {
 
     public static boolean isNotLongerAgoThen(@NonNull Date date, int minutes, int seconds) {
         long differenceInMs = new Date().getTime() - date.getTime();
-        return differenceInMs <= (minutes * 60 + seconds) * 1000;
+        return differenceInMs <= (minutes * 60 + seconds) * 1000L;
     }
 }
