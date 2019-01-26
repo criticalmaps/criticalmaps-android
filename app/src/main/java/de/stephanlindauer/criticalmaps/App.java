7a94956734c35b30becca049ac4f440e4ee7005a
Christian Balster <christian.balster@gmail.com>
2019-01-26 15:35:28 +0100

    Add LeakCanary
    release build uses no-op version

--- app/src/main/java/de/stephanlindauer/criticalmaps/App.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/App.java
@@ -2,6 +2,8 @@ package de.stephanlindauer.criticalmaps;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import org.jetbrains.annotations.NotNull;
 
 import timber.log.Timber;
@@ -13,6 +15,13 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
