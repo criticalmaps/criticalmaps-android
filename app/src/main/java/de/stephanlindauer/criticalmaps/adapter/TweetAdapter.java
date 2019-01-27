4a902de66e601540dcd9cc995e10b5996188e1bc
Christian Balster <christian.balster@gmail.com>
2019-01-27 13:57:17 +0100

    Rework tweet view

--- app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TweetAdapter.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TweetAdapter.java
@@ -83,8 +83,8 @@ public class TweetAdapter extends ArrayAdapter<Tweet> {
 
         viewHolder.nameTextView.setText(tweet.getUserName());
         viewHolder.textTextView.setText(Html.fromHtml(tweet.getText()).toString());
-        viewHolder.dateTextView.setText(new SimpleDateFormat("HH:mm", Locale.US).format(tweet.getTimestamp()));
-        viewHolder.timeTextView.setText(new SimpleDateFormat("dd.MM.yyyy", Locale.US).format(tweet.getTimestamp()));
+        viewHolder.timeTextView.setText(new SimpleDateFormat("HH:mm", Locale.US).format(tweet.getTimestamp()));
+        viewHolder.dateTextView.setText(new SimpleDateFormat("dd.MM.yyyy", Locale.US).format(tweet.getTimestamp()));
         viewHolder.handleTextView.setText("@" + tweet.getUserScreenName());
 
         rowView.setOnClickListener(new View.OnClickListener() {
