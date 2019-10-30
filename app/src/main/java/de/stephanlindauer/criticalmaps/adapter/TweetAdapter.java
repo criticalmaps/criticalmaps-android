fbb72fb9cf7af80e8f494f0cd07d2643866ac0aa
askar <askar.syzdykov@gmail.com>
2019-10-31 00:26:17 +0600

    Twitter redesign: Circle avatar, date/time formatting. Bump

--- app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TweetAdapter.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/adapter/TweetAdapter.java
@@ -3,27 +3,26 @@ package de.stephanlindauer.criticalmaps.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import androidx.annotation.NonNull;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
-import com.squareup.picasso.Callback;
-
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.stephanlindauer.criticalmaps.App;
 import de.stephanlindauer.criticalmaps.R;
 import de.stephanlindauer.criticalmaps.model.twitter.Tweet;
+import de.stephanlindauer.criticalmaps.views.CircleTransformation;
 
 public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetViewHolder> {
 
@@ -31,22 +30,23 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetViewHol
     private List<Tweet> tweets;
 
     public static class TweetViewHolder extends RecyclerView.ViewHolder {
+
+        private final static long DAY_MS = 86_400_400;
+        private final static long HOUR_MS = 3_600_600;
+        private final static long MINUTE_MS = 60_000;
+
+        private final Context context;
+
         @BindView(R.id.tweet_user_name)
         TextView nameTextView;
         @BindView(R.id.tweet_text)
         TextView textTextView;
-        @BindView(R.id.tweet_creation_date)
-        TextView dateTextView;
-        @BindView(R.id.tweet_creation_time)
-        TextView timeTextView;
+        @BindView(R.id.tweet_creation_date_time)
+        TextView dateTimeTextView;
         @BindView(R.id.tweet_user_handle)
         TextView handleTextView;
         @BindView(R.id.tweet_user_image)
         ImageView userImageView;
-        @BindView(R.id.tweet_user_image_progress)
-        ProgressBar userImageProgress;
-
-        private final Context context;
 
         public TweetViewHolder(Context context, @NonNull View itemView) {
             super(itemView);
@@ -55,38 +55,36 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.TweetViewHol
         }
 
         public void bind(Tweet tweet) {
-            userImageProgress.setVisibility(View.VISIBLE);
             App.components().picasso().load(tweet.getProfileImageUrl())
                     .fit()
                     .centerInside()
+                    .transform(new CircleTransformation())
                     .error(R.drawable.ic_about_twitter)
-                    .into(userImageView, new Callback() {
-                        @Override
-                        public void onSuccess() {
-                            hideProgressBar();
-                        }
-
-                        @Override
-                        public void onError() {
-                            hideProgressBar();
-                        }
-
-                        private void hideProgressBar() {
-                            final ProgressBar progressBar = userImageProgress;
-                            if (progressBar != null) {
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        }
-                    });
+                    .placeholder(R.drawable.twitter_avatar_placeholder)
+                    .into(userImageView);
 
             nameTextView.setText(tweet.getUserName());
             textTextView.setText(Html.fromHtml(tweet.getText()).toString());
-            timeTextView.setText(
-                    new SimpleDateFormat("HH:mm", Locale.US).format(tweet.getTimestamp()));
-            dateTextView.setText(
-                    new SimpleDateFormat("dd.MM.yyyy", Locale.US).format(tweet.getTimestamp()));
+
+            final long currentDateMs = new Date().getTime();
+            final long tweetDateMs = tweet.getTimestamp().getTime();
+            final long delta = currentDateMs - tweetDateMs;
+            final int days = (int) (delta / DAY_MS);
+            final int hours = (int) (delta / HOUR_MS);
+            final int minutes = (int) ((delta / MINUTE_MS) % 60);
+            if (days > 10) {
+                dateTimeTextView.setText(new SimpleDateFormat("dd.MMM", Locale.US).format(tweetDateMs));
+            }
+            if (days > 0 && days < 10) {
+                dateTimeTextView.setText(String.format("%d d", days));
+            } else if (hours > 0 && hours < 24) {
+                dateTimeTextView.setText(String.format("%d h", hours));
+            } else {
+                dateTimeTextView.setText(String.format("%d min", minutes));
+            }
+
             handleTextView.setText(String.format(
-                    context.getString(R.string.twitter_handle),tweet.getUserScreenName()));
+                    context.getString(R.string.twitter_handle), tweet.getUserScreenName()));
 
             itemView.setOnClickListener(v ->
                     context.startActivity(new Intent(Intent.ACTION_VIEW,
