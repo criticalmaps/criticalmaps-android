93426d3644a2752be9aa95c376ca8a22f9ca2178
Christian Balster <christian.balster@gmail.com>
2019-12-14 18:35:08 +0100

    Test cleanup

--- app/src/androidTest/java/de/stephanlindauer/criticalmaps/ApplicationTest.java
+++ app/src/androidTest/java/de/stephanlindauer/criticalmaps/ApplicationTest.java
@@ -2,18 +2,15 @@ package de.stephanlindauer.criticalmaps;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-@RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ApplicationTest {
 
--- app/src/test/java/de/stephanlindauer/criticalmaps/utils/AeSimpleSHA1Test.java
+++ app/src/test/java/de/stephanlindauer/criticalmaps/utils/AeSimpleSHA1Test.java
@@ -2,14 +2,11 @@ package de.stephanlindauer.criticalmaps.utils;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-
 import static com.google.common.truth.Truth.assertThat;
 
 public class AeSimpleSHA1Test {
     @Test
-    public void testThatSHA1IsCalculatedCorrectly() throws URISyntaxException, FileNotFoundException {
+    public void testThatSHA1IsCalculatedCorrectly() {
         final String sha1ForProbe = AeSimpleSHA1.SHA1("probe");
         assertThat(sha1ForProbe).isEqualTo("a949c530710f9fca76b45776267c68967fa891e7");
     }
