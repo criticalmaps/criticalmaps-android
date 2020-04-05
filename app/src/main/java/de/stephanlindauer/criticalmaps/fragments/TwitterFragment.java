7d03f695935b2a5d40016e31b2bfdac25352438b
Christian Balster <christian.balster@gmail.com>
2020-04-05 19:13:19 +0200

    Migrate twitter fragment from butterknife to view binding
    plus some small refactorings

--- app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/fragments/TwitterFragment.java
@@ -4,96 +4,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 import de.stephanlindauer.criticalmaps.App;
 import de.stephanlindauer.criticalmaps.R;
 import de.stephanlindauer.criticalmaps.adapter.TweetAdapter;
+import de.stephanlindauer.criticalmaps.databinding.FragmentTwitterBinding;
 import de.stephanlindauer.criticalmaps.handler.PostTweetHandler;
 import de.stephanlindauer.criticalmaps.handler.TwitterGetHandler;
 import de.stephanlindauer.criticalmaps.model.TwitterModel;
 
 public class TwitterFragment extends Fragment {
 
-    //dependencies
     private final TwitterModel twitterModel = App.components().twitterModel();
 
-    //view
-    @BindView(R.id.twitter_error)
-    LinearLayout errorMessage;
-
-    @BindView(R.id.twitter_error_button)
-    Button errorButton;
-
-    @BindView(R.id.tweet_list)
-    RecyclerView tweetsRecyclerView;
-
-    @BindView(R.id.swipe_container)
-    public SwipeRefreshLayout swipeLayout;
-
-    @BindView(R.id.twitter_new_tweet_button)
-    public FloatingActionButton newTweetButton;
-
-    //adapter
     private TweetAdapter tweetAdapter;
-    private Unbinder unbinder;
+    private FragmentTwitterBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_twitter, container, false);
-        unbinder = ButterKnife.bind(this, view);
 
-        tweetsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        return view;
+        binding = FragmentTwitterBinding.inflate(inflater, container, false);
+        binding.twitterTweetsRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(final Bundle savedState) {
         super.onActivityCreated(savedState);
 
-        tweetAdapter = new TweetAdapter(getActivity(), new ArrayList<>());
-        tweetsRecyclerView.setAdapter(tweetAdapter);
-        tweetsRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        tweetAdapter = new TweetAdapter(new ArrayList<>());
+        binding.twitterTweetsRecyclerview.setAdapter(tweetAdapter);
+        binding.twitterTweetsRecyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                if (dy > 0 && newTweetButton.getVisibility() == View.VISIBLE) {
-                    newTweetButton.hide();
-                } else if (dy < 0 && newTweetButton.getVisibility() != View.VISIBLE) {
-                    newTweetButton.show();
+                if (dy > 0 && binding.twitterNewTweetButton.getVisibility() == View.VISIBLE) {
+                    binding.twitterNewTweetButton.hide();
+                } else if (dy < 0 && binding.twitterNewTweetButton.getVisibility() != View.VISIBLE) {
+                    binding.twitterNewTweetButton.show();
                 }
             }
         });
-        swipeLayout.setOnRefreshListener(() ->
+
+        binding.twitterContainerLayout.setOnRefreshListener(() ->
                 new TwitterGetHandler(TwitterFragment.this).execute());
 
-        swipeLayout.setColorSchemeResources(
+        binding.twitterContainerLayout.setColorSchemeResources(
                 R.color.twitter_indicator_color_first,
                 R.color.twitter_indicator_color_second);
 
-        swipeLayout.setProgressBackgroundColorSchemeResource(R.color.tweet_progress_bar_background);
+        binding.twitterContainerLayout.setProgressBackgroundColorSchemeResource(
+                R.color.tweet_progress_bar_background);
 
-        errorButton.setOnClickListener(v -> {
+        binding.twitterErrorButton.setOnClickListener(v -> {
             new TwitterGetHandler(TwitterFragment.this).execute();
-            errorMessage.setVisibility(View.GONE);
+            binding.twitterErrorLayout.setVisibility(View.GONE);
         });
+
+        binding.twitterNewTweetButton.setOnClickListener(v ->
+                new PostTweetHandler(getActivity()).execute());
 
         new TwitterGetHandler(this).execute();
     }
@@ -101,24 +80,23 @@ public class TwitterFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unbinder.unbind();
+        binding = null;
     }
 
     public void displayNewData() {
-        swipeLayout.setRefreshing(false);
-        errorMessage.setVisibility(View.GONE);
+        binding.twitterContainerLayout.setRefreshing(false);
+        binding.twitterErrorLayout.setVisibility(View.GONE);
 
-        swipeLayout.setVisibility(View.VISIBLE);
+        binding.twitterContainerLayout.setVisibility(View.VISIBLE);
         tweetAdapter.updateData(twitterModel.getTweets());
     }
 
     public void showErrorMessage() {
-        swipeLayout.setVisibility(View.GONE);
-        errorMessage.setVisibility(View.VISIBLE);
+        binding.twitterContainerLayout.setVisibility(View.GONE);
+        binding.twitterErrorLayout.setVisibility(View.VISIBLE);
     }
 
-    @OnClick(R.id.twitter_new_tweet_button)
-    void handleNewTweetClicked(View view) {
-        new PostTweetHandler(getActivity()).execute();
+    public void setRefreshing(boolean refreshing) {
+        binding.twitterContainerLayout.setRefreshing(refreshing);
     }
 }
