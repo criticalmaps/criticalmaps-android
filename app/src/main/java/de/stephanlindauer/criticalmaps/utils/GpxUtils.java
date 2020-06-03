bb563e85bce3d26618316ed2f88dffc200767b5d
Stefan Kinzel <9340853+stefanctdo@users.noreply.github.com>
2020-06-03 18:29:34 +0200

    Remove persisting of unnecessary permission

--- app/src/main/java/de/stephanlindauer/criticalmaps/utils/GpxUtils.java
+++ app/src/main/java/de/stephanlindauer/criticalmaps/utils/GpxUtils.java
@@ -7,9 +7,7 @@ import android.os.Build;
 public class GpxUtils {
 
     public static void persistPermissionOnFile(Intent data, ContentResolver contentResolver) {
-        final int permissionFlags = data.getFlags()
-                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        final int permissionFlags = data.getFlags() & Intent.FLAG_GRANT_READ_URI_PERMISSION;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && data.getData() != null) {
             contentResolver.takePersistableUriPermission(data.getData(), permissionFlags);
         }
