b0d4bf39dd907fd0016ae89d9875a957186ec953
Christian Balster <christian.balster@gmail.com>
2023-02-05 17:39:18 +0100

    Update unit tests and test dependencies

--- app/src/test/java/de/stephanlindauer/criticalmaps/handler/ServerResponseProcessorTest.java
+++ app/src/test/java/de/stephanlindauer/criticalmaps/handler/ServerResponseProcessorTest.java
@@ -3,11 +3,14 @@ package de.stephanlindauer.criticalmaps.handler;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
-import org.ligi.axt.AXT;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 import de.stephanlindauer.criticalmaps.events.Events;
 import de.stephanlindauer.criticalmaps.model.ChatModel;
@@ -24,8 +27,8 @@ public class ServerResponseProcessorTest {
     @Test
     public void process_chatmessagesAreSetOnModel() throws IOException, URISyntaxException,
             JSONException {
-        final String json = AXT.at(new File(getClass().getClassLoader()
-                .getResource("simple_server_response.json").toURI())).readToString();
+        final String json = readToString(new File(getClass().getClassLoader()
+                .getResource("simple_server_response.json").toURI()));
         final ChatModel chatModel = mock(ChatModel.class);
         final ServerResponseProcessor tested = new ServerResponseProcessor(
                 mock(OtherUsersLocationModel.class), mock(EventBus.class), chatModel);
@@ -37,8 +40,8 @@ public class ServerResponseProcessorTest {
 
     @Test
     public void process_eventIsFiredForValidJSON() throws IOException, URISyntaxException {
-        final String json = AXT.at(new File(getClass().getClassLoader()
-                .getResource("simple_server_response.json").toURI())).readToString();
+        final String json = readToString(new File(getClass().getClassLoader()
+                .getResource("simple_server_response.json").toURI()));
         final EventBus eventMock = mock(EventBus.class);
         final ServerResponseProcessor tested = new ServerResponseProcessor(
                 mock(OtherUsersLocationModel.class), eventMock, mock(ChatModel.class));
@@ -57,5 +60,28 @@ public class ServerResponseProcessorTest {
         tested.process("borken");
 
         verify(eventMock, never()).post(Events.NEW_SERVER_RESPONSE_EVENT);
+    }
+
+    public String readToString(File file) throws IOException {
+        return readToString(Charset.defaultCharset(), file);
+    }
+
+    public String readToString(Charset charset, File file) throws IOException {
+        final FileInputStream stream = new FileInputStream(file);
+        try {
+            return readToStringFromFileInputStream(charset, stream);
+        } finally {
+            stream.close();
+        }
+    }
+
+    private String readToStringFromFileInputStream(Charset charset, FileInputStream stream) throws IOException {
+        final FileChannel fc = stream.getChannel();
+        try {
+            final MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            return charset.decode(bb).toString();
+        } finally {
+            fc.close();
+        }
     }
 }
