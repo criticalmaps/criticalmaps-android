66d663b2f35a3a5a7d14f24319faae1e18712ce2
Christian Balster <christian.balster@gmail.com>
2025-11-18 22:58:22 +0100

    Set up Material3 theme
    No dynamic color support and default to dark theme for now

---
+++ app/src/main/java/de/stephanlindauer/criticalmaps/ui/theme/Type.kt
@@ -0,0 +1,29 @@
+package de.stephanlindauer.criticalmaps.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
+
+val bodyFontFamily = FontFamily.Default
+
+val displayFontFamily = FontFamily.Serif
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val AppTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+)
